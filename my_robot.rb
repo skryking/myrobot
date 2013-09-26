6db9d01d1f43b538155cbc5b2247d6b7ea9e1341
Jason Ormes <skryking@gmail.com>
2013-09-26 15:40:29 -0500

    Removed the rest of LED stuff

--- my_robot.rb
+++ my_robot.rb
@@ -2,7 +2,6 @@ require 'artoo'
 
 connection :firmata, :adaptor => :firmata, :port => '127.0.0.1:8023'
 device :board
-device :led, :driver => :led, :pin => 13
 
 work do
   puts "Firmware name #{board.firmware_name}"
