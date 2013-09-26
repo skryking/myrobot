2595a261abd6ab913fbc8071f008f37c1d120f4e
Jason Ormes <skryking@gmail.com>
2013-09-26 15:36:53 -0500

    Removing led flash

--- my_robot.rb
+++ my_robot.rb
@@ -7,7 +7,5 @@ device :led, :driver => :led, :pin => 13
 work do
   puts "Firmware name #{board.firmware_name}"
   puts "Firmata version #{board.version}"
-  every 1.second do
-    led.toggle
-  end
+
 end
