22a86747649717b5ad53da073fcb5b44327e60bf
Jason Ormes <skryking@gmail.com>
2013-09-30 21:12:55 -0500

    moving a servo

--- my_robot.rb
+++ my_robot.rb
@@ -2,9 +2,19 @@ require 'artoo'
 
 connection :firmata, :adaptor => :firmata, :port => '/dev/ttyACM0'
 device :board
+device :servo, :driver => :servo, :pin => 3 # pin must be a PWM pin
+which = false
 
 work do
   puts "Firmware name #{board.firmware_name}"
   puts "Firmata version #{board.version}"
 
+  if which 
+    servo.move(100)
+  else
+      servo.move(70)
+ end
+ 
+ which != which
+ sleep(5)
 end
